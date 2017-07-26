b141144512f6371f8e1dfac541a80a0d4bfe378c
stanleykayzz <ondzoungabruce@hotmail.fr>
2017-07-26 15:31:08 +0100

    modification du restaurantRepository

--- src/main/java/server/repository/RestaurantTableRepository.java
+++ src/main/java/server/repository/RestaurantTableRepository.java
@@ -4,11 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import server.model.RestaurantTable;
 
+import java.util.List;
+
 @Component
 public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Long>{
 
 
-   // RestaurantTable findById(int number);
+    RestaurantTable findById(int number);
 
     RestaurantTable findByNumber(String number);
+
+    List<RestaurantTable> findByNumberChairs(String numberOfChairs);
 }
