712d42d8dafb3b69f14b7896463bb76f0884419f
maximem75 <mollard.maxime75@hotmail.fr>
2017-09-22 13:26:02 +0200

    [API] mail test

--- src/main/java/server/utils/Mail/MailManager.java
+++ src/main/java/server/utils/Mail/MailManager.java
@@ -1,7 +1,12 @@
 package server.utils.Mail;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
-import java.util.*;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 /**
  * Created by maxime on 22/09/2017.
  */
@@ -26,6 +31,24 @@ public class MailManager {
                         return new javax.mail.PasswordAuthentication(username, password);
                     }
                 });
+    }
+
+    public void sendEmailToClient(String clientEmail){
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("alvin.ondzounga@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(clientEmail));
+            message.setSubject("Confirmation de réservation RHM");
+            //String email = convertHtmlFileIntoString("C:\\Users\\stanley\\Downloads\\Spring-Email-Gmail-Smtp-Example\\ModulesIntelligents\\src\\invoiceHobby.html");
+            String email = "test";
+            System.out.println("On a "+email);
+            message.setContent(email,"text/html");
+            Transport.send(message);
+            System.out.println("Done");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
