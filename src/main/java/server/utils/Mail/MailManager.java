575f12b4835a772a2cbec25d1ac748b7beaf9ec8
maximem75 <mollard.maxime75@hotmail.fr>
2017-09-24 22:15:35 +0200

    test

--- src/main/java/server/utils/Mail/MailManager.java
+++ src/main/java/server/utils/Mail/MailManager.java
@@ -22,8 +22,8 @@ public class MailManager {
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "465");
-        //props.put("mail.smtp.port", "587");
+        //props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.port", "587");
         props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
         session = Session.getInstance(props,
