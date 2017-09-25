bd4f4429d2af1fe54a8a500660714ff2a94f6ce9
maximem75 <mollard.maxime75@hotmail.fr>
2017-09-25 19:09:10 +0200

    [front] update style

--- src/main/java/server/controller/BuildingController.java
+++ src/main/java/server/controller/BuildingController.java
@@ -1,21 +1,16 @@
 package server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import server.model.Building;
 import server.repository.BuildingRepository;
-import server.service.BuildingService;
 import server.service.ClientService;
-import server.service.RoomService;
 
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.*;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
-//@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/building")
 public class BuildingController {
